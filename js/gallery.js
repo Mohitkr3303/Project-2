554479a3652ffcf330c14b45fe78f73ba49bcea2
Mohitkr3303 <142003980+Mohitkr3303@users.noreply.github.com>
2023-08-12 13:43:55 -0400

    Add files via upload

---
+++ js/gallery.js
@@ -0,0 +1,12 @@
+
+      var swiper = new Swiper(".mySwiper2", {
+        slidesPerView: 3,
+        grid: {
+          rows: 2,
+        },
+        spaceBetween: 30,
+        pagination: {
+          el: ".swiper-pagination2",
+          clickable: true,
+        },
+      });
